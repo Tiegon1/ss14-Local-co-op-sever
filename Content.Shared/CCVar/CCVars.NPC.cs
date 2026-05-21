a09ed378ac1dc75d1d5d414f2e31954aebe6767a
Tiegon <tiegonb@gmail.com>
2026-05-20 23:02:58 -0400

    more npc updates means less braindead npcs but we might need to fix this if there are bad side effects

--- Content.Shared/CCVar/CCVars.NPC.cs
+++ Content.Shared/CCVar/CCVars.NPC.cs
@@ -11,7 +11,7 @@ namespace Content.Shared.CCVar;
 public sealed partial class CCVars
 {
     public static readonly CVarDef<int> NPCMaxUpdates =
-        CVarDef.Create("npc.max_updates", 128);
+        CVarDef.Create("npc.max_updates", 300);
 
     public static readonly CVarDef<bool> NPCEnabled = CVarDef.Create("npc.enabled", true);
 
